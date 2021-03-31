a4ce1e7a1adff1494231116d9fd29a91c648e22f
rsanchezavalos <r.sanchezavalos@gmail.com>
2021-03-31 13:56:48 -0600

    initial commit

---
+++ utils.R
@@ -0,0 +1,6 @@
+
+list.of.packages <- c('shiny', 'leaflet', 'osrm', 'rgdal',
+                      'sp', 'sf', 'viridis', 'shiny',
+                      'leaflet', 'shinythemes')
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
